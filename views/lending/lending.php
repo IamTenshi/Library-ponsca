001aeefb2c3bbc1cd0bb33391d0027531371282c
IamTenshi <angeljimenezabreu2@gmail.com>
2023-05-12 12:34:54 -0400

    He añadido el sistema de reportes

--- views/lending/lending.php
+++ views/lending/lending.php
@@ -203,6 +203,9 @@
                             </div>
 
                             <button id="new-user-button" class="bg-sidebar hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mb-2">New Lending</button>
+                            <a href="reportLendings.php">
+                                <button id="lending-report-button" class="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded mb-2">Report</button>
+                            </a>
 
                             <div id="new-user-form" class="bg-white p-6 rounded-lg shadow-md" style="display: none;">
                                 <form action="addLending.php" method="post">
